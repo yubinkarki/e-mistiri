e4d761055988cc95d99cb2831c83ceb3fca9d56f
yubinkarki <iamyubinkarki@gmail.com>
2023-01-02 21:42:36 +0545

    feat: Custom input field design

--- src/commons/InputField.js
+++ src/commons/InputField.js
@@ -1,10 +1,45 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {TextInput} from 'react-native-paper';
+import {Colors} from '@app/constants';
 
-export default function InputField() {
+export default function InputField({
+  labelText,
+  isPassword,
+  passwordIcon,
+  onBlur,
+  onChange,
+  value,
+  outlineColor = 'transparent',
+}) {
+  const [hidePassword, setHidePassword] = useState(isPassword);
+  const [eyeIcon, setEyeIcon] = useState(passwordIcon);
+
   return (
-    <View>
-      <Text>InputField</Text>
-    </View>
+    <TextInput
+      theme={{roundness: 12}}
+      mode="outlined"
+      label={labelText}
+      secureTextEntry={hidePassword}
+      outlineColor={outlineColor}
+      activeOutlineColor={Colors.textLink}
+      onBlur={onBlur}
+      onChangeText={onChange}
+      value={value}
+      selectionColor={Colors.subtitleText}
+      style={{backgroundColor: Colors.white}}
+      right={
+        passwordIcon && (
+          <TextInput.Icon
+            forceTextInputFocus={false}
+            icon={eyeIcon ? 'eye-off-outline' : 'eye'}
+            color={Colors.inputFieldIcon}
+            onPress={() => {
+              setHidePassword(!hidePassword);
+              setEyeIcon(!eyeIcon);
+            }}
+          />
+        )
+      }
+    />
   );
 }
