d33678b9187ab9ffb4973efe0c4b25c9fac352b1
yubinkarki <iamyubinkarki@gmail.com>
2023-01-02 21:42:12 +0545

    Add new colors for components

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -5,4 +5,7 @@ export default {
   onboardingNextButtonTopGradient: 'rgba(33, 46, 126, 0.91)',
   onboardingNextButtonBotGradient: 'rgba(33, 46, 126, 0)',
   splashScreenBG: '#41416E',
+  textLink: '#212E7E',
+  subtitleText: '#8F93AF',
+  inputFieldIcon: '#868686',
 };
