d3d972d9f536486aa5addb9a4763e8f58ee0aa76
yubinkarki <iamyubinkarki@gmail.com>
2023-01-02 21:42:49 +0545

    feat: Custom button design

--- src/commons/PrimaryButton.js
+++ src/commons/PrimaryButton.js
@@ -1,10 +1,56 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
+import MCI from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Colors, TextStyles} from '@app/constants';
 
-export default function PrimaryButton() {
+const Styles = StyleSheet.create({
+  buttonStyles: {
+    width: 'auto',
+  },
+  labelStyles: {
+    ...TextStyles.poppinsExtraLargeNormal,
+  },
+  contentStyles: {
+    height: '100%',
+  },
+});
+
+export default function PrimaryButton({
+  buttonLabel,
+  buttonHeight,
+  onPressHandler,
+  buttonWidth,
+  buttonIconName,
+  buttonIconSize,
+  buttonIconColor,
+  buttonRadius,
+  isOutlined,
+}) {
   return (
-    <View>
-      <Text>PrimaryButton</Text>
-    </View>
+    <Button
+      mode={isOutlined ? 'outlined' : 'contained'}
+      color={Colors.textLink}
+      uppercase={false}
+      style={{
+        ...Styles.buttonStyles,
+        height: buttonHeight,
+        width: buttonWidth,
+        borderRadius: buttonRadius,
+      }}
+      labelStyle={Styles.labelStyles}
+      contentStyle={Styles.contentStyles}
+      icon={() => (
+        <MCI
+          name={buttonIconName}
+          size={buttonIconSize}
+          color={buttonIconColor}
+        />
+      )}
+      onPress={() => {
+        onPressHandler();
+      }}>
+      {buttonLabel}
+    </Button>
   );
 }
