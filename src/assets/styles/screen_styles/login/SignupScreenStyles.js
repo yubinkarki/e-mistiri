c6d8b2fa16b5a34524a1a95dd7cb848ce421109a
yubinkarki <iamyubinkarki@gmail.com>
2023-01-04 21:57:37 +0545

    feat: Signup screen design complete

--- src/assets/styles/screen_styles/login/SignupScreenStyles.js
+++ src/assets/styles/screen_styles/login/SignupScreenStyles.js
@@ -15,7 +15,7 @@ export const SignupScreenStyles = StyleSheet.create({
     alignItems: 'center',
   },
   mainLogoContainer: {
-    paddingTop: hp('10%'),
+    paddingTop: hp('8%'),
     paddingBottom: hp('4%'),
   },
   titleText: {
@@ -28,5 +28,46 @@ export const SignupScreenStyles = StyleSheet.create({
     color: Colors.subtitleText,
     textAlign: 'center',
     paddingTop: hp('1%'),
+  },
+  formContainer: {
+    marginTop: hp('5%'),
+  },
+  signupButtonContainer: {
+    marginTop: hp('4%'),
+  },
+  inputFieldContainer: {
+    height: hp('9%'),
+  },
+  errorText: {
+    ...TextStyles.poppinsSmallLight,
+    color: Colors.errorText,
+    paddingLeft: wp('4.4%'),
+    paddingTop: hp('0.2%'),
+  },
+  privacyPolicyContainer: {
+    flexDirection: 'row',
+    marginTop: hp('2%'),
+  },
+  policyText: {
+    ...TextStyles.poppinsRegularLight,
+    color: Colors.subtitleText,
+    paddingRight: wp('15%'),
+  },
+  policyLinkText: {
+    ...TextStyles.poppinsRegularLight,
+    color: Colors.textLink,
+  },
+  signupFooterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: hp('6%'),
+  },
+  footerText: {
+    ...TextStyles.poppinsRegularLight,
+    color: Colors.footerText,
+  },
+  footerLinkText: {
+    ...TextStyles.poppinsRegularLight,
+    color: Colors.textLink,
   },
 });
--- src/screens/login/signup_screens/Signup.js
+++ src/screens/login/signup_screens/Signup.js
@@ -1,13 +1,19 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, StatusBar} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+  ScrollView,
+} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {Checkbox} from 'react-native-paper';
+import {inputRules} from '../components';
 import {Colors, Images} from '@app/constants';
 import {InputField, PrimaryButton} from '@app/commons';
 import {SignupScreenStyles as Styles} from '@app/assets/styles';
 
-export default function Signup() {
+export default function Signup({navigation}) {
   const {
     control,
     handleSubmit,
@@ -28,9 +34,8 @@ export default function Signup() {
   };
 
   return (
-    <KeyboardAwareScrollView
+    <ScrollView
       style={Styles.mainContainer}
-      enableOnAndroid={false}
       keyboardShouldPersistTaps="handled">
       <StatusBar backgroundColor={Colors.loginBackground} />
 
@@ -45,6 +50,125 @@ export default function Signup() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         </Text>
       </View>
-    </KeyboardAwareScrollView>
+
+      <View style={Styles.formContainer}>
+        <View style={Styles.inputFieldContainer}>
+          <Controller
+            control={control}
+            name="email"
+            rules={{...inputRules.email}}
+            render={({field: {onChange, onBlur, value}}) => (
+              <InputField
+                labelText="Email Address"
+                isPassword={false}
+                onBlur={onBlur}
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          {errors?.email?.type === 'required' ? (
+            <Text style={Styles.errorText}>Enter your email</Text>
+          ) : errors?.email?.type === 'pattern' ? (
+            <Text style={Styles.errorText}>Enter a valid email address</Text>
+          ) : null}
+        </View>
+
+        <View style={Styles.inputFieldContainer}>
+          <Controller
+            control={control}
+            name="password"
+            rules={{...inputRules.password}}
+            render={({field: {onChange, onBlur, value}}) => (
+              <InputField
+                labelText="Password"
+                isPassword={true}
+                passwordIcon={true}
+                onBlur={onBlur}
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          {errors?.password?.type === 'required' ? (
+            <Text style={Styles.errorText}>Enter your password</Text>
+          ) : errors?.password?.type === 'pattern' ? (
+            <Text style={Styles.errorText}>Password pattern doesn't match</Text>
+          ) : errors?.password?.type === 'minLength' ? (
+            <Text style={Styles.errorText}>Too short</Text>
+          ) : null}
+        </View>
+
+        <View style={Styles.inputFieldContainer}>
+          <Controller
+            control={control}
+            name="confirmPassword"
+            rules={{
+              ...inputRules.confirmPassword,
+              validate: value =>
+                value === watch('password') || 'The passwords do not match',
+            }}
+            render={({field: {onChange, onBlur, value}}) => (
+              <InputField
+                labelText="Confirm Password"
+                isPassword={true}
+                passwordIcon={true}
+                onBlur={onBlur}
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          {errors?.confirmPassword?.type === 'required' ? (
+            <Text style={Styles.errorText}>Confirm your password</Text>
+          ) : null}
+        </View>
+      </View>
+
+      <View style={Styles.privacyPolicyContainer}>
+        <Checkbox
+          status={checked ? 'checked' : 'unchecked'}
+          onPress={() => {
+            setChecked(!checked);
+          }}
+          uncheckedColor={Colors.checkBox}
+          color={Colors.textLink}
+        />
+
+        <Text style={Styles.policyText}>
+          I agree with the
+          <Text style={Styles.policyLinkText} onPress={() => {}}>
+            Terms of Service
+          </Text>{' '}
+          and{' '}
+          <Text style={Styles.policyLinkText} onPress={() => {}}>
+            Privacy Policy.
+          </Text>
+        </Text>
+      </View>
+
+      <View style={Styles.signupButtonContainer}>
+        <PrimaryButton
+          buttonRadius={10}
+          buttonLabel="Sign Up"
+          buttonHeight={55}
+          handleSubmit={handleSubmit}
+          onPressHandler={handleSubmit(signupSubmitHandler)}
+        />
+      </View>
+
+      <View style={Styles.signupFooterContainer}>
+        <Text style={Styles.footerText}>Already have an account? </Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('LoginPage');
+          }}>
+          <Text style={Styles.footerLinkText}>Signin</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
