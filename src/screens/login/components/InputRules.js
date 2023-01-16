253238e319f7b5a67e161000b3c97648c0287fed
yubinkarki <iamyubinkarki@gmail.com>
2023-01-16 09:15:36 +0545

    Add value and message

--- src/screens/login/components/InputRules.js
+++ src/screens/login/components/InputRules.js
@@ -1,26 +1,43 @@
 export const inputRules = {
   email: {
-    required: true,
+    required: {
+      value: true,
+      message: 'Please provide an email address',
+    },
     pattern: {
       value:
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      message: 'This email address is not valid',
     },
   },
   fullName: {
-    required: true,
-    minLength: 6,
-    pattern: {
-      value: /^([A-Za-z]{3,}) ([A-Za-z]{3,})$/,
+    required: {
+      value: true,
+      message: 'Please enter your full name',
+    },
+    minLength: {
+      value: 6,
+      message: 'Minimum 6 characters required',
     },
   },
   password: {
-    required: true,
-    minLength: 8,
+    required: {
+      value: true,
+      message: 'Create a new password',
+    },
+    minLength: {
+      value: 8,
+      message: 'Minimum 8 characters required',
+    },
     pattern: {
       value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      message: 'Password too weak',
     },
   },
   confirmPassword: {
-    required: true,
+    required: {
+      value: true,
+      message: 'Please confirm your password',
+    },
   },
 };
