4dc91d7cc3ae112099b95e71b9af1c055fb92ee8
yubinkarki <iamyubinkarki@gmail.com>
2023-01-19 21:34:34 +0545

    Move controller to input component

--- src/commons/InputField.js
+++ src/commons/InputField.js
@@ -1,52 +1,77 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
+import {Controller} from 'react-hook-form';
 import {TextInput} from 'react-native-paper';
-import {Colors} from '@app/constants';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {Colors, TextStyles} from '@app/constants';
 
 const Styles = StyleSheet.create({
   textInput: {
     backgroundColor: Colors.inputField,
   },
+  errorText: {
+    ...TextStyles.poppinsSmallLight,
+    color: Colors.errorText,
+    paddingLeft: wp('4.4%'),
+    paddingTop: hp('0.2%'),
+  },
 });
 
 export default function InputField({
+  control,
+  errors,
+  rules,
+  inputName,
   labelText,
-  isPassword,
   passwordIcon,
+  isPassword = false,
   outlineColor = 'transparent',
-  onBlur,
-  onChange,
-  value,
 }) {
   const [hidePassword, setHidePassword] = useState(isPassword);
   const [eyeIcon, setEyeIcon] = useState(passwordIcon);
 
   return (
-    <TextInput
-      theme={{roundness: 12}}
-      mode="outlined"
-      placeholder={labelText}
-      secureTextEntry={hidePassword}
-      outlineColor={outlineColor}
-      activeOutlineColor={Colors.textLink}
-      onBlur={onBlur}
-      onChangeText={onChange}
-      value={value}
-      selectionColor={Colors.subtitleText}
-      style={Styles.textInput}
-      right={
-        passwordIcon && (
-          <TextInput.Icon
-            forceTextInputFocus={false}
-            icon={eyeIcon ? 'eye-off-outline' : 'eye'}
-            color={Colors.inputFieldIcon}
-            onPress={() => {
-              setHidePassword(!hidePassword);
-              setEyeIcon(!eyeIcon);
-            }}
+    <>
+      <Controller
+        control={control}
+        rules={rules}
+        name={inputName}
+        render={({field: {onBlur, onChange, value}}) => (
+          <TextInput
+            theme={{roundness: 12}}
+            mode="outlined"
+            placeholder={labelText}
+            secureTextEntry={hidePassword}
+            outlineColor={outlineColor}
+            activeOutlineColor={Colors.textLink}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            selectionColor={Colors.subtitleText}
+            style={Styles.textInput}
+            right={
+              passwordIcon && (
+                <TextInput.Icon
+                  forceTextInputFocus={false}
+                  icon={eyeIcon ? 'eye-off-outline' : 'eye'}
+                  color={Colors.inputFieldIcon}
+                  onPress={() => {
+                    setHidePassword(!hidePassword);
+                    setEyeIcon(!eyeIcon);
+                  }}
+                />
+              )
+            }
           />
-        )
-      }
-    />
+        )}
+      />
+
+      {errors[inputName] && (
+        <Text style={Styles.errorText}>{errors[inputName]?.message}</Text>
+      )}
+    </>
   );
 }
