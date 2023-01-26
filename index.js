db23af60c82bc15517e4c6bc9f8704abc64bb835
yubinkarki <iamyubinkarki@gmail.com>
2023-01-26 20:09:25 +0545

    Wrap with persistor and store

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from '@app/redux/Store';
 
-AppRegistry.registerComponent(appName, () => App);
+function Root() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Root);
