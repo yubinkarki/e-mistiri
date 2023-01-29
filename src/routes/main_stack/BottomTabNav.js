1f1860d9c1b00c48bbdcf4bf1b1c9413ea6a24f2
yubinkarki <iamyubinkarki@gmail.com>
2023-01-29 11:04:18 +0545

    Bottom tab style finalized

--- src/routes/main_stack/BottomTabNav.js
+++ src/routes/main_stack/BottomTabNav.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import DashboardStack from './DashboardStack';
-import {Colors, Images} from '@app/constants';
+import ShopStack from './ShopStack';
+import CartStack from './CartStack';
+import AccountStack from './AccountStack';
+import {Colors, Images, TextStyles} from '@app/constants';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
@@ -11,65 +14,14 @@ export default function BottomTabNav() {
   return (
     <Tab.Navigator
       screenOptions={({route}) => ({
-        tabBarIcon: ({focused}) => {
-          let iconName;
-
-          switch (route.name) {
-            case 'DashboardStack':
-              iconName = focused ? (
-                <Ionicons name="home" size={23} color={Colors.textLink} />
-              ) : (
-                <Ionicons
-                  name="home"
-                  size={23}
-                  color={Colors.productCardSubtitle}
-                />
-              );
-              break;
-
-            case 'ShopStack':
-              iconName = focused ? (
-                <Images.shopIcon style={{color: Colors.textLink}} />
-              ) : (
-                <Images.shopIcon style={{color: Colors.productCardSubtitle}} />
-              );
-              break;
-
-            case 'CartStack':
-              iconName = focused ? (
-                <Images.cartIcon style={{color: Colors.textLink}} />
-              ) : (
-                <Images.cartIcon style={{color: Colors.productCardSubtitle}} />
-              );
-              break;
-
-            case 'AccountStack':
-              iconName = focused ? (
-                <Images.accountIcon style={{color: Colors.textLink}} />
-              ) : (
-                <Images.accountIcon
-                  style={{color: Colors.productCardSubtitle}}
-                />
-              );
-              break;
-
-            default:
-              break;
-          }
-
-          return iconName;
-        },
-        tabBarStyle: {
-          borderTopRightRadius: 20,
-          borderTopLeftRadius: 20,
-          marginHorizontal: 5,
-          position: 'absolute',
-          overflow: 'hidden',
-          bottom: 0,
-        },
-        tabBarLabelStyle: {bottom: 4},
+        tabBarIcon: ({focused}) => checkIconStatus(focused, route),
+        tabBarStyle: Styles.tabBar,
+        tabBarLabelStyle: Styles.tabBarLabel,
         tabBarActiveTintColor: Colors.textLink,
         tabBarInactiveTintColor: Colors.productCardSubtitle,
+        headerTitleAlign: 'center',
+        headerTitleStyle: Styles.headerTitle,
+        headerStyle: Styles.header,
       })}>
       <Tab.Screen
         name="DashboardStack"
@@ -79,21 +31,85 @@ export default function BottomTabNav() {
 
       <Tab.Screen
         name="ShopStack"
-        component={DashboardStack}
-        options={{title: 'Shop'}}
+        component={ShopStack}
+        options={{title: 'Shop', headerTitle: 'All Products'}}
       />
 
       <Tab.Screen
         name="CartStack"
-        component={DashboardStack}
+        component={CartStack}
         options={{title: 'Cart'}}
       />
 
       <Tab.Screen
         name="AccountStack"
-        component={DashboardStack}
+        component={AccountStack}
         options={{title: 'Account', headerShown: false}}
       />
     </Tab.Navigator>
   );
 }
+
+const checkIconStatus = (focused, route) => {
+  let iconName;
+
+  switch (route.name) {
+    case 'DashboardStack':
+      iconName = focused ? (
+        <Ionicons name="home" size={22} color={Colors.textLink} />
+      ) : (
+        <Ionicons name="home" size={22} color={Colors.productCardSubtitle} />
+      );
+      break;
+
+    case 'ShopStack':
+      iconName = focused ? (
+        <Images.shopIcon style={{color: Colors.textLink}} />
+      ) : (
+        <Images.shopIcon style={{color: Colors.productCardSubtitle}} />
+      );
+      break;
+
+    case 'CartStack':
+      iconName = focused ? (
+        <Images.cartIcon style={{color: Colors.textLink}} />
+      ) : (
+        <Images.cartIcon style={{color: Colors.productCardSubtitle}} />
+      );
+      break;
+
+    case 'AccountStack':
+      iconName = focused ? (
+        <Images.accountIcon style={{color: Colors.textLink}} />
+      ) : (
+        <Images.accountIcon style={{color: Colors.productCardSubtitle}} />
+      );
+      break;
+
+    default:
+      break;
+  }
+
+  return iconName;
+};
+
+const Styles = StyleSheet.create({
+  tabBar: {
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    marginHorizontal: 8,
+    position: 'absolute',
+    overflow: 'hidden',
+    bottom: 0,
+  },
+  tabBarLabel: {
+    bottom: 4,
+  },
+  header: {
+    elevation: 0,
+  },
+  headerTitle: {
+    ...TextStyles.poppinsExtraLargeBold,
+    fontSize: 24,
+  },
+});
