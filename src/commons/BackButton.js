a11eb0186650555bd73dfcc47572d486856d520e
yubinkarki <iamyubinkarki@gmail.com>
2023-01-29 14:58:35 +0545

    Create header back button

--- src/commons/BackButton.js
+++ src/commons/BackButton.js
@@ -1,10 +1,26 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import AD from 'react-native-vector-icons/AntDesign';
+import {Colors} from '@app/constants';
 
-export default function BackButton() {
+export default function BackButton({onPress}) {
   return (
     <View>
-      <Text>BackButton</Text>
+      <TouchableOpacity
+        style={Styles.backButton}
+        onPress={onPress}
+        activeOpacity={0.5}>
+        <AD name="left" size={22} color={Colors.splashScreenBG} />
+      </TouchableOpacity>
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  backButton: {
+    backgroundColor: Colors.white,
+    borderRadius: 8,
+    padding: 10,
+    elevation: 7,
+  },
+});
