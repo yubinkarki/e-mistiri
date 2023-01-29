c67265c4d142c69de066deda17bdf6f142ad5f5c
yubinkarki <iamyubinkarki@gmail.com>
2023-01-29 11:03:24 +0545

    Add new  imports

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,15 +1,19 @@
+// App logo.
 import MainLogo from '../assets/svg/logo/MainLogo.svg';
 import MainLogoColor from '../assets/svg/logo/MainLogoColor.svg';
 
+// Icons for bottom tab navigation bar.
 import Home from '../assets/svg/bottom_nav/Home.svg';
 import Cart from '../assets/svg/bottom_nav/Cart.svg';
 import Account from '../assets/svg/bottom_nav/Account.svg';
 import Shop from '../assets/svg/bottom_nav/Shop.svg';
 
+// Onboarding images.
 import Onboarding1 from '../assets/svg/onboarding/Onboarding1.svg';
 import Onboarding2 from '../assets/svg/onboarding/Onboarding2.svg';
 import Onboarding3 from '../assets/svg/onboarding/Onboarding3.svg';
 
+// Dashboard screen images.
 import SearchIcon from '../assets/svg/dashboard/SearchIcon.svg';
 import FilterButtonIcon from '../assets/svg/dashboard/FilterButtonIcon.svg';
 import Bike from '../assets/svg/dashboard/Bike.svg';
@@ -17,9 +21,20 @@ import Car from '../assets/svg/dashboard/Car.svg';
 import Scooter from '../assets/svg/dashboard/Scooter.svg';
 import Accessories from '../assets/svg/dashboard/Accessories.svg';
 
+// Product images.
 import BrakePad from '../assets/svg/product/BrakePad.svg';
 import CastrolMagnatec from '../assets/svg/product/CastrolMagnatec.svg';
 import EngineFilter from '../assets/svg/product/EngineFilter.svg';
+
+// Account screen images.
+import DefaultProfile from '../assets/images/DefaultProfile.png';
+import RewardPointRibbon from '../assets/svg/account/RewardPointRibbon.svg';
+import AboutUs from '../assets/svg/account/AboutUs.svg';
+import Logout from '../assets/svg/account/Logout.svg';
+import MyInformation from '../assets/svg/account/MyInformation.svg';
+import Settings from '../assets/svg/account/Settings.svg';
+import Share from '../assets/svg/account/Share.svg';
+import RightArrow from '../assets/svg/account/RightArrow.svg';
 
 export default {
   mainLogo: MainLogo,
@@ -40,4 +55,12 @@ export default {
   cartIcon: Cart,
   accountIcon: Account,
   shopIcon: Shop,
+  defaultProfile: DefaultProfile,
+  rewardPointRibbon: RewardPointRibbon,
+  aboutUs: AboutUs,
+  logout: Logout,
+  myInformation: MyInformation,
+  settings: Settings,
+  share: Share,
+  rightArrow: RightArrow,
 };
