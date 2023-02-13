4b03662a0c51c65101723746792660dd8ed16c0e
yubinkarki <iamyubinkarki@gmail.com>
2023-02-13 09:49:07 +0545

    Favorite button design

--- src/commons/FavoriteButton.js
+++ src/commons/FavoriteButton.js
@@ -1,10 +1,28 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {TouchableOpacity, StyleSheet} from 'react-native';
+import AD from 'react-native-vector-icons/AntDesign';
+
+import {Colors} from '@app/constants';
 
 export default function FavoriteButton() {
+  const [iconName, setIconName] = useState('hearto');
+
   return (
-    <View>
-      <Text>FavoriteButton</Text>
-    </View>
+    <TouchableOpacity
+      style={Styles.mainContainer}
+      onPress={() => {
+        iconName === 'hearto' ? setIconName('heart') : setIconName('hearto');
+      }}>
+      <AD name={iconName} size={22} color={Colors.splashScreenBG} />
+    </TouchableOpacity>
   );
 }
+
+const Styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: Colors.white,
+    borderRadius: 8,
+    padding: 10,
+    elevation: 7,
+  },
+});
