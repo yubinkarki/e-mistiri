7aa907c8cb25197cf597d447cf85ba101c6de55d
yubinkarki <iamyubinkarki@gmail.com>
2023-02-22 21:28:51 +0545

    Add new reducer and cleared state

--- src/redux/slices/cart_info/CartInfo.js
+++ src/redux/slices/cart_info/CartInfo.js
@@ -3,35 +3,7 @@ import {createSlice} from '@reduxjs/toolkit';
 import {Images} from '@app/constants';
 
 const initialState = {
-  cartProducts: [
-    {
-      id: 1,
-      title: 'Engine Filter',
-      subtitle: 'Yamaha',
-      price: 1000,
-      discountedPrice: null,
-      image: <Images.engineFilter width={'90%'} height={'90%'} />,
-      count: 2,
-    },
-    {
-      id: 2,
-      title: 'Regular Servicing',
-      subtitle: 'The Wheels Pvt Ltd',
-      price: 1240,
-      discountedPrice: 980,
-      image: <Images.regularService />,
-      count: 1,
-    },
-    {
-      id: 3,
-      title: 'Brake Pad',
-      subtitle: 'Royal Enfield',
-      price: 640,
-      discountedPrice: null,
-      image: <Images.brakePad width={'100%'} height={'100%'} />,
-      count: 3,
-    },
-  ],
+  cartProducts: [],
 };
 
 export const CartInfo = createSlice({
@@ -53,9 +25,13 @@ export const CartInfo = createSlice({
         item => item.id !== action.payload,
       );
     },
+    addProduct: (state, action) => {
+      state.cartProducts = [...state.cartProducts, action.payload];
+    },
   },
 });
 
-export const {increaseCount, decreaseCount, removeCartItem} = CartInfo.actions;
+export const {increaseCount, decreaseCount, removeCartItem, addProduct} =
+  CartInfo.actions;
 
 export default CartInfo.reducer;
--- src/redux/slices/cart_info/index.js
+++ src/redux/slices/cart_info/index.js
@@ -2,6 +2,13 @@ import CartInfoSlice, {
   increaseCount,
   decreaseCount,
   removeCartItem,
+  addProduct,
 } from './CartInfo';
 
-export {CartInfoSlice, increaseCount, decreaseCount, removeCartItem};
+export {
+  CartInfoSlice,
+  increaseCount,
+  decreaseCount,
+  removeCartItem,
+  addProduct,
+};
