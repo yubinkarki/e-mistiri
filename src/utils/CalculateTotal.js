c66a2b10b3f5caea06113eff42eb69ae64a6c8e3
yubinkarki <iamyubinkarki@gmail.com>
2023-02-24 23:10:02 +0545

    Optional chain

--- src/utils/CalculateTotal.js
+++ src/utils/CalculateTotal.js
@@ -1,13 +1,13 @@
 export const totalAmount = data => {
   return data
-    .map(item =>
-      item.discountedPrice
-        ? item.discountedPrice * item.count
-        : item.price * item.count,
+    ?.map(item =>
+      item?.discountedPrice || 0
+        ? item?.discountedPrice * item?.count
+        : item?.price * item?.count,
     )
     .reduce((a, b) => a + b, 0);
 };
 
 export const totalCount = data => {
-  return data.map(item => item.count).reduce((a, b) => a + b, 0);
+  return data?.map(item => item?.count || 0).reduce((a, b) => a + b, 0);
 };
