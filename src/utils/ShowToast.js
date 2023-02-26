53631650ebf6225b851b682e8ca007b5da90e660
yubinkarki <iamyubinkarki@gmail.com>
2023-02-26 20:38:06 +0545

    Remove visibility time

--- src/utils/ShowToast.js
+++ src/utils/ShowToast.js
@@ -6,7 +6,6 @@ export default ShowToast = data =>
     type: data?.type || 'info',
     text1: data?.title || 'Title',
     text2: data?.subtitle || 'Subtitle',
-    visibilityTime: data?.visibilityTime || 3000,
     topOffset: hp(data?.offset || '1%'),
     position: data?.position || 'top',
     onPress: () => Toast.hide(),
